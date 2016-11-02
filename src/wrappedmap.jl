f43a800d8f8d57c7611716baed548f355db153b2
Jutho Haegeman <jutho.haegeman@ugent.be>
2016-11-02 23:45:42 +0100

    update wrappedmap

--- src/wrappedmap.jl
+++ src/wrappedmap.jl
@@ -1,37 +1,42 @@
-immutable WrappedMap{T}<:AbstractLinearMap{T}
-    lmap::Union{AbstractMatrix{T},AbstractLinearMap{T}}
+immutable WrappedMap{T,A} <: AbstractLinearMap{T}
+    lmap::A
     _isreal::Bool
     _issymmetric::Bool
     _ishermitian::Bool
     _isposdef::Bool
-    WrappedMap(A::Union{AbstractMatrix{T},AbstractLinearMap{T}};isreal::Bool=Base.isreal(A),issymmetric::Bool=Base.issymmetric(A),ishermitian::Bool=Base.ishermitian(A),isposdef::Bool=Base.isposdef(A))=new(A,isreal,issymmetric,ishermitian,isposdef)
 end
-WrappedMap{T}(A::Union{AbstractMatrix{T},AbstractLinearMap{T}};isreal::Bool=Base.isreal(A),issymmetric::Bool=Base.issymmetric(A),ishermitian::Bool=Base.ishermitian(A),isposdef::Bool=Base.isposdef(A))=WrappedMap{T}(A;isreal=isreal,issymmetric=issymmetric,ishermitian=ishermitian,isposdef=isposdef)
+(::Type{WrappedMap}){T,A<:Union{AbstractMatrix,AbstractLinearMap}}(lmap::A, ::Type{T} = eltype(lmap);
+    isreal::Bool = Base.isreal(lmap), issymmetric::Bool = Base.issymmetric(lmap),
+    ishermitian::Bool = Base.ishermitian(lmap), isposdef::Bool = Base.isposdef(lmap)) =
+        WrappedMap{T,A}(lmap, isreal, issymmetric, ishermitian, isposdef)
 
 # properties
-Base.size(A::WrappedMap,n)=(n==1 || n==2 ? size(A.lmap,n) : error("AbstractLinearMap objects have only 2 dimensions"))
-Base.size(A::WrappedMap)=size(A.lmap)
-Base.isreal(A::WrappedMap)=A._isreal
-Base.issymmetric(A::WrappedMap)=A._issymmetric
-Base.ishermitian(A::WrappedMap)=A._ishermitian
-Base.isposdef(A::WrappedMap)=A._isposdef
+Base.size(A::WrappedMap,n) = (n==1 || n==2 ? size(A.lmap,n) : error("AbstractLinearMap objects have only 2 dimensions"))
+Base.size(A::WrappedMap) = size(A.lmap)
+Base.isreal(A::WrappedMap) = A._isreal
+Base.issymmetric(A::WrappedMap) = A._issymmetric
+Base.ishermitian(A::WrappedMap) = A._ishermitian
+Base.isposdef(A::WrappedMap) = A._isposdef
 
 # comparison
-==(A::WrappedMap,B::WrappedMap)=(A.lmap==B.lmap && isreal(A)==isreal(B) && issymmetric(A)==issymmetric(B) && ishermitian(A)==ishermitian(B) && isposdef(A)==isposdef(B))
+==(A::WrappedMap,B::WrappedMap) = (A.lmap==B.lmap && isreal(A)==isreal(B) &&
+    issymmetric(A)==issymmetric(B) && ishermitian(A)==ishermitian(B) && isposdef(A)==isposdef(B))
 
 # multiplication with vector
-Base.A_mul_B!(y::AbstractVector,A::WrappedMap,x::AbstractVector)=Base.A_mul_B!(y,A.lmap,x)
-*(A::WrappedMap,x::AbstractVector)=*(A.lmap,x)
+Base.A_mul_B!(y::AbstractVector, A::WrappedMap,x::AbstractVector) = Base.A_mul_B!(y, A.lmap, x)
+*(A::WrappedMap, x::AbstractVector) = *(A.lmap,x)
 
-Base.At_mul_B!(y::AbstractVector,A::WrappedMap,x::AbstractVector)=Base.At_mul_B!(y,A.lmap,x)
-Base.At_mul_B(A::WrappedMap,x::AbstractVector)=Base.At_mul_B(A.lmap,x)
+Base.At_mul_B!(y::AbstractVector, A::WrappedMap,x::AbstractVector) = Base.At_mul_B!(y, A.lmap, x)
+Base.At_mul_B(A::WrappedMap,x::AbstractVector) = Base.At_mul_B(A.lmap, x)
 
-Base.Ac_mul_B!(y::AbstractVector,A::WrappedMap,x::AbstractVector)=Base.Ac_mul_B!(y,A.lmap,x)
-Base.Ac_mul_B(A::WrappedMap,x::AbstractVector)=Base.Ac_mul_B(A.lmap,x)
+Base.Ac_mul_B!(y::AbstractVector,A::WrappedMap,x::AbstractVector) = Base.Ac_mul_B!(y, A.lmap, x)
+Base.Ac_mul_B(A::WrappedMap,x::AbstractVector) = Base.Ac_mul_B(A.lmap, x)
 
 # combine AbstractLinearMap and Matrix objects: linear combinations and map composition
-+(A1::AbstractLinearMap,A2::AbstractMatrix)=+(A1,WrappedMap(A2))
-+(A1::AbstractMatrix,A2::AbstractLinearMap)=+(WrappedMap(A1),A2)
++(A1::AbstractLinearMap, A2::AbstractMatrix) = +(A1, WrappedMap(A2))
++(A1::AbstractMatrix, A2::AbstractLinearMap) = +(WrappedMap(A1), A2)
+-(A1::AbstractLinearMap, A2::AbstractMatrix) = -(A1, WrappedMap(A2))
+-(A1::AbstractMatrix, A2::AbstractLinearMap) = -(WrappedMap(A1), A2)
 
-*(A1::AbstractLinearMap,A2::AbstractMatrix)=*(A1,WrappedMap(A2))
-*(A1::AbstractMatrix,A2::AbstractLinearMap)=*(WrappedMap(A1),A2)
+*(A1::AbstractLinearMap, A2::AbstractMatrix) = *(A1, WrappedMap(A2))
+*(A1::AbstractMatrix, A2::AbstractLinearMap) = *(WrappedMap(A1) ,A2)
