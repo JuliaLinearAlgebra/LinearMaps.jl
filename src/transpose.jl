4b3dd74473bd46875b519d9951f9db67676d8f65
Jutho <jutho.haegeman@ugent.be>
2016-11-03 10:02:00 +0100

    make transpose typestable

--- src/transpose.jl
+++ src/transpose.jl
@@ -11,12 +11,13 @@ end
 Base.transpose(A::TransposeMap)=A.lmap
 Base.ctranspose(A::CTransposeMap)=A.lmap
 
-Base.transpose(A::AbstractLinearMap) = issymmetric(A) ? A : TransposeMap(A)
+Base.transpose(A::AbstractLinearMap) = TransposeMap(A)
 Base.ctranspose{T<:Real}(A::AbstractLinearMap{T}) = transpose(A)
-Base.ctranspose(A::AbstractLinearMap) = ishermitian(A) ? A : CTransposeMap(A)
+Base.ctranspose(A::AbstractLinearMap) = CTransposeMap(A)
 
 # properties
-Base.size(A::Union{TransposeMap,CTransposeMap},n) = (n==1 ? size(A.lmap,2) : (n==2 ? size(A.lmap,1) : error("AbstractLinearMap objects have only 2 dimensions")))
+Base.size(A::Union{TransposeMap,CTransposeMap},n) = (n==1 ? size(A.lmap,2) :
+    (n==2 ? size(A.lmap,1) : error("AbstractLinearMap objects have only 2 dimensions")))
 Base.size(A::Union{TransposeMap,CTransposeMap}) = (size(A.lmap,2), size(A.lmap,1))
 Base.isreal(A::Union{TransposeMap,CTransposeMap}) = isreal(A.lmap)
 Base.issymmetric(A::Union{TransposeMap,CTransposeMap}) = issymmetric(A.lmap)
@@ -24,23 +25,31 @@ Base.ishermitian(A::Union{TransposeMap,CTransposeMap}) = ishermitian(A.lmap)
 Base.isposdef(A::Union{TransposeMap,CTransposeMap}) = isposdef(A.lmap)
 
 # comparison of TransposeMap objects
-==(A::TransposeMap,B::TransposeMap) = A.lmap == B.lmap
-==(A::CTransposeMap,B::CTransposeMap) = A.lmap == B.lmap
+==(A::TransposeMap, B::TransposeMap) = A.lmap == B.lmap
+==(A::CTransposeMap, B::CTransposeMap) = A.lmap == B.lmap
+==(A::TransposeMap, B::AbstractLinearMap) = issymmetric(B) && A.lmap == B
+==(A::CTransposeMap, B::AbstractLinearMap) = ishermitian(B) && A.lmap == B
+==(A::AbstractLinearMap, B::TransposeMap) = issymmetric(A) && B.lmap == A
+==(A::AbstractLinearMap, B::CTransposeMap) = ishermitian(A) && B.lmap == A
 
 # multiplication with vector
-Base.A_mul_B!(y::AbstractVector, A::TransposeMap, x::AbstractVector) = Base.At_mul_B!(y, A.lmap, x)
-*(A::TransposeMap,x::AbstractVector) = Base.At_mul_B(A.lmap, x)
+Base.A_mul_B!(y::AbstractVector, A::TransposeMap, x::AbstractVector) =
+    (issymmetric(A.lmap) ? Base.A_mul_B!(y,A.lmap,x) : Base.At_mul_B!(y,A.lmap,x))
+*(A::TransposeMap, x::AbstractVector) =
+    (issymmetric(A.lmap) ? *(A.lmap,x) : Base.At_mul_B(A.lmap,x))
 
-Base.At_mul_B!(y::AbstractVector, A::TransposeMap, x::AbstractVector) = Base.A_mul_B!(y,A.lmap,x)
-Base.At_mul_B(A::TransposeMap, x::AbstractVector) = *(A.lmap,x)
+Base.At_mul_B!(y::AbstractVector, A::TransposeMap, x::AbstractVector) =
+    Base.A_mul_B!(y,A.lmap,x)
+Base.At_mul_B(A::TransposeMap, x::AbstractVector) =
+    *(A.lmap,x)
 
 Base.Ac_mul_B!(y::AbstractVector, A::TransposeMap, x::AbstractVector) =
-    isreal(A.lmap) ? Base.A_mul_B!(y,A.lmap,x) : (Base.A_mul_B!(y,A.lmap,conj(x)); conj!(y))
-Base.Ac_mul_B(A::TransposeMap, x::AbstractVector) =
-    isreal(A.lmap) ? *(A.lmap,x) : conj!(*(A.lmap,conj(x)))
+    isreal(A.lmap) ? Base.A_mul_B!(y,A.lmap,x) : (Base.A_mul_B!(y,A.lmap,conj(x));conj!(y))
+Base.Ac_mul_B(A::TransposeMap, x::AbstractVector) = isreal(A.lmap) ? *(A.lmap,x) : conj!(*(A.lmap,conj(x)))
 
-Base.A_mul_B!(y::AbstractVector, A::CTransposeMap, x::AbstractVector) = Base.Ac_mul_B!(y,A.lmap,x)
-*(A::CTransposeMap, x::AbstractVector) = Base.Ac_mul_B(A.lmap,x)
+Base.A_mul_B!(y::AbstractVector, A::CTransposeMap, x::AbstractVector) =
+    (ishermitian(A.lmap) ? Base.A_mul_B!(y,A.lmap,x) : Base.Ac_mul_B!(y,A.lmap,x))
+*(A::CTransposeMap,x::AbstractVector) = (ishermitian(A.lmap) ? *(A.lmap,x) : Base.Ac_mul_B(A.lmap,x))
 
 Base.At_mul_B!(y::AbstractVector, A::CTransposeMap, x::AbstractVector) =
     isreal(A.lmap) ? Base.A_mul_B!(y,A.lmap,x) : (Base.A_mul_B!(y,A.lmap,conj(x));conj!(y))
