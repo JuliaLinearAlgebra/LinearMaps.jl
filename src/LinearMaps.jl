352cd64d4a853669a4bbad2960e888e5ef96fc6c
ScottPJones <scottjones@alum.mit.edu>
2017-05-16 12:09:51 -0400

    Updates for v0.6 compatibility

--- src/LinearMaps.jl
+++ src/LinearMaps.jl
@@ -4,7 +4,8 @@ export LinearMap, AbstractLinearMap
 
 import Base: +, -, *, \, /, ==
 
-abstract AbstractLinearMap{T}
+eval(Expr(:abstract, :(AbstractLinearMap{T} <: Any)))
+
 Base.eltype{T}(::AbstractLinearMap{T})=T
 Base.eltype{T}(::Type{AbstractLinearMap{T}})=T
 Base.eltype{L<:AbstractLinearMap}(::Type{L})=eltype(super(L))
@@ -17,11 +18,14 @@ Base.ishermitian(::AbstractLinearMap) = false # standard assumptions
 Base.isposdef(::AbstractLinearMap) = false # standard assumptions
 
 Base.ndims(::AbstractLinearMap) = 2
-Base.size(A::AbstractLinearMap, n) = (n==1 || n==2 ? size(A)[n] : error("AbstractLinearMap objects have only 2 dimensions"))
+Base.size(A::AbstractLinearMap, n) =
+    (n==1 || n==2 ? size(A)[n] : error("AbstractLinearMap objects have only 2 dimensions"))
 Base.length(A::AbstractLinearMap) = reduce(*, size(A))
 
-# any AbstractLinearMap subtype will have to overwrite at least one of the two following methods to avoid running in circles
-*(A::AbstractLinearMap,x::AbstractVector)=Base.A_mul_B!(similar(x,promote_type(eltype(A),eltype(x)),size(A,1)),A,x)
+# any AbstractLinearMap subtype will have to overwrite at least one of the two following methods
+# to avoid running in circles
+*(A::AbstractLinearMap,x::AbstractVector) =
+    Base.A_mul_B!(similar(x,promote_type(eltype(A),eltype(x)),size(A,1)),A,x)
 Base.A_mul_B!(y::AbstractVector,A::AbstractLinearMap,x::AbstractVector)=begin
     length(y)==size(A,1) || throw(DimensionMismatch("A_mul_B!"))
     copy!(y,A*x)
@@ -29,15 +33,20 @@ end
 
 # the following for multiplying with transpose and ctranspose map are optional:
 # subtypes can overwrite nonmutating methods, implement mutating methods or do nothing
-Base.At_mul_B(A::AbstractLinearMap,x::AbstractVector)=(@which Base.At_mul_B!(x,A,x))!=methods(Base.At_mul_B!,(AbstractVector,AbstractLinearMap,AbstractVector))[end] ?
+Base.At_mul_B(A::AbstractLinearMap,x::AbstractVector) =
+    (@which Base.At_mul_B!(x,A,x))!=methods(Base.At_mul_B!,(AbstractVector,AbstractLinearMap,AbstractVector))[end] ?
     Base.At_mul_B!(similar(x,promote_type(eltype(A),eltype(x)),size(A,2)),A,x) : throw(MethodError(Base.At_mul_B,(A,x)))
-Base.At_mul_B!(y::AbstractVector,A::AbstractLinearMap,x::AbstractVector)=begin
+
+Base.At_mul_B!(y::AbstractVector,A::AbstractLinearMap,x::AbstractVector) = begin
     length(y)==size(A,2) || throw(DimensionMismatch("At_mul_B!"))
     (@which Base.At_mul_B(A,x))!=methods(Base.At_mul_B,(AbstractLinearMap,AbstractVector))[end] ? copy!(y,Base.At_mul_B(A,x)) : throw(MethodError(Base.At_mul_B!,(y,A,x)))
 end
-Base.Ac_mul_B(A::AbstractLinearMap,x::AbstractVector)=(@which Base.Ac_mul_B!(x,A,x))!=methods(Base.Ac_mul_B!,(AbstractVector,AbstractLinearMap,AbstractVector))[end] ?
+
+Base.Ac_mul_B(A::AbstractLinearMap,x::AbstractVector) =
+    (@which Base.Ac_mul_B!(x,A,x))!=methods(Base.Ac_mul_B!,(AbstractVector,AbstractLinearMap,AbstractVector))[end] ?
     Base.Ac_mul_B!(similar(x,promote_type(eltype(A),eltype(x)),size(A,2)),A,x) : throw(MethodError(Base.Ac_mul_B,(A,x)))
-Base.Ac_mul_B!(y::AbstractVector,A::AbstractLinearMap,x::AbstractVector)=begin
+
+Base.Ac_mul_B!(y::AbstractVector,A::AbstractLinearMap,x::AbstractVector) = begin
     length(y)==size(A,2) || throw(DimensionMismatch("At_mul_B!"))
     (@which Base.Ac_mul_B(A,x))!=methods(Base.Ac_mul_B,(AbstractLinearMap,AbstractVector))[end] ? copy!(y,Base.Ac_mul_B(A,x)) : throw(MethodError(Base.Ac_mul_B!,(y,A,x)))
 end
