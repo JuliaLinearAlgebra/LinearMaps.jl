7b43cd6d82f290053a578a096eb3fff174867cd4
yikait2 <46879771+yikait2@users.noreply.github.com>
2019-06-06 03:30:39 -0500

    Testings on Octonions (#41)

--- test/runtests.jl
+++ test/runtests.jl
@@ -423,9 +423,25 @@ end
     @test Array(α * L) == α * A
     @test Array(L * α) == A * α
     @test Array(α * L) == α * A
-    @test Array(L * α ) == A * α
+    @test Array(L * α) == A * α
     @test (α * L')' * x ≈ (α * A')' * x
     @test (α * L')' * v ≈ (α * A')' * v
     @test Array(@inferred adjoint(α * L * β)) ≈ conj(β) * A' * conj(α)
     @test Array(@inferred transpose(α * L * β)) ≈ β * transpose(A) * α
+end
+
+@testset "nonassociative number type" begin
+    using Quaternions
+    x = Octonion.(rand(10), rand(10), rand(10), rand(10),rand(10), rand(10), rand(10), rand(10))
+    v = rand(10)
+    A = Octonion.(rand(10,10), rand(10,10), rand(10,10), rand(10,10),rand(10,10), rand(10,10), rand(10,10), rand(10,10))
+    α = UniformScaling(Octonion.(rand(8)...))
+    β = UniformScaling(Octonion.(rand(8)...))
+    L = LinearMap(A)
+    @test Array(L) == A
+    @test Array(α * L) == α * A
+    @test Array(L * α) == A * α
+    @test Array(α * L) == α * A
+    @test Array(L * α) == A * α
+    @test (α * L')' * v ≈ (α * A')' * v
 end
