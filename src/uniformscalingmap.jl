f669025d70bfe848eba4c2a08bea767798bbde0a
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-07-10 07:53:16 +0200

    speed up mul with UniformScalingMap, inherit type from LinearMap

--- src/uniformscalingmap.jl
+++ src/uniformscalingmap.jl
@@ -19,20 +19,48 @@ LinearAlgebra.transpose(A::UniformScalingMap) = A
 LinearAlgebra.adjoint(A::UniformScalingMap)   = UniformScalingMap(conj(A.λ), size(A))
 
 # multiplication with vector
+Base.:(*)(A::UniformScalingMap, x::AbstractVector) = A.λ * x
 # call of LinearAlgebra.generic_mul! since order of arguments in mul! in stdlib/LinearAlgebra/src/generic.jl
 # TODO: either leave it as is or use mul! (and lower bound on version) once fixed in LinearAlgebra
-A_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector) =
-    (length(x) == length(y) == A.M ? LinearAlgebra.generic_mul!(y, A.λ, x) : throw(DimensionMismatch("A_mul_B!")))
-Base.:(*)(A::UniformScalingMap, x::AbstractVector) = A.λ * x
+function A_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
+    (length(x) == length(y) == A.M || throw(DimensionMismatch("A_mul_B!")))
+    if iszero(A.λ)
+        return fill!(y, 0)
+    elseif isone(A.λ)
+        return copyto!(y, x)
+    else
+        return LinearAlgebra.generic_mul!(y, A.λ, x)
+    end
+end
 
-At_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector) =
-    (length(x) == length(y) == A.M ? LinearAlgebra.generic_mul!(y, A.λ, x) : throw(DimensionMismatch("At_mul_B!")))
+function At_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
+    (length(x) == length(y) == A.M || throw(DimensionMismatch("At_mul_B!")))
+    if iszero(A.λ)
+        return fill!(y, 0)
+    elseif isone(A.λ)
+        return copyto!(y, x)
+    else
+        return LinearAlgebra.generic_mul!(y, A.λ, x)
+    end
+end
 
-Ac_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector) =
-    (length(x) == length(y) == A.M ? LinearAlgebra.generic_mul!(y, conj(A.λ), x) : throw(DimensionMismatch("Ac_mul_B!")))
+function Ac_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
+    (length(x) == length(y) == A.M || throw(DimensionMismatch("At_mul_B!")))
+    if iszero(A.λ)
+        return fill!(y, 0)
+    elseif isone(A.λ)
+        return copyto!(y, x)
+    else
+        return LinearAlgebra.generic_mul!(y, conj(A.λ), x)
+    end
+end
 
 # combine LinearMap and UniformScaling objects in linear combinations
-Base.:(+)(A1::LinearMap, A2::UniformScaling{T}) where {T} = A1 + UniformScalingMap{T}(A2[1,1], size(A1, 1))
-Base.:(+)(A1::UniformScaling{T}, A2::LinearMap) where {T} = UniformScalingMap{T}(A1[1,1], size(A2, 1)) + A2
-Base.:(-)(A1::LinearMap, A2::UniformScaling{T}) where {T} = A1 - UniformScalingMap{T}(A2[1,1], size(A1, 1))
-Base.:(-)(A1::UniformScaling{T}, A2::LinearMap) where {T} = UniformScalingMap{T}(A1[1,1], size(A2, 1)) - A2
+Base.:(+)(A1::LinearMap{T}, A2::UniformScaling) where {T} =
+    A1 + UniformScalingMap(convert(T, A2[1,1]), size(A1, 1))
+Base.:(+)(A1::UniformScaling, A2::LinearMap{T}) where {T} =
+    UniformScalingMap(convert(T, A1[1,1]), size(A2, 1)) + A2
+Base.:(-)(A1::LinearMap{T}, A2::UniformScaling) where {T} =
+    A1 - UniformScalingMap(convert(T, A2[1,1]), size(A1, 1))
+Base.:(-)(A1::UniformScaling, A2::LinearMap{T}) where {T} =
+    UniformScalingMap(convert(T, A1[1,1]), size(A2, 1)) - A2
