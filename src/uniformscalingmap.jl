f48075676490b6c2bb4ed4ac03ab0ed03ee25f99
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-07-18 21:06:36 +0200

    update as per review

--- src/uniformscalingmap.jl
+++ src/uniformscalingmap.jl
@@ -19,7 +19,9 @@ LinearAlgebra.transpose(A::UniformScalingMap) = A
 LinearAlgebra.adjoint(A::UniformScalingMap)   = UniformScalingMap(conj(A.λ), size(A))
 
 # multiplication with vector
-Base.:(*)(A::UniformScalingMap, x::AbstractVector) = A.λ * x
+Base.:(*)(A::UniformScalingMap, x::AbstractVector) =
+    length(x) == A.M ? A.λ * x : throw(DimensionMismatch("A_mul_B!"))
+
 # call of LinearAlgebra.generic_mul! since order of arguments in mul! in stdlib/LinearAlgebra/src/generic.jl
 # TODO: either leave it as is or use mul! (and lower bound on version) once fixed in LinearAlgebra
 function A_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
@@ -32,35 +34,15 @@ function A_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
         return LinearAlgebra.generic_mul!(y, A.λ, x)
     end
 end
-
-function At_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
-    (length(x) == length(y) == A.M || throw(DimensionMismatch("At_mul_B!")))
-    if iszero(A.λ)
-        return fill!(y, 0)
-    elseif isone(A.λ)
-        return copyto!(y, x)
-    else
-        return LinearAlgebra.generic_mul!(y, A.λ, x)
-    end
-end
-
-function Ac_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector)
-    (length(x) == length(y) == A.M || throw(DimensionMismatch("At_mul_B!")))
-    if iszero(A.λ)
-        return fill!(y, 0)
-    elseif isone(A.λ)
-        return copyto!(y, x)
-    else
-        return LinearAlgebra.generic_mul!(y, conj(A.λ), x)
-    end
-end
+At_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector) = A_mul_B!(y, transpose(A), x)
+Ac_mul_B!(y::AbstractVector, A::UniformScalingMap, x::AbstractVector) = A_mul_B!(y, adjoint(A), x)
 
 # combine LinearMap and UniformScaling objects in linear combinations
 Base.:(+)(A1::LinearMap{T}, A2::UniformScaling) where {T} =
-    A1 + UniformScalingMap(convert(T, A2[1,1]), size(A1, 1))
+    A1 + UniformScalingMap(convert(promote_type(T, eltype(A2)), A2.λ), size(A1, 1))
 Base.:(+)(A1::UniformScaling, A2::LinearMap{T}) where {T} =
-    UniformScalingMap(convert(T, A1[1,1]), size(A2, 1)) + A2
+    UniformScalingMap(convert(promote_type(eltype(A1), T), A1.λ), size(A2, 1)) + A2
 Base.:(-)(A1::LinearMap{T}, A2::UniformScaling) where {T} =
-    A1 - UniformScalingMap(convert(T, A2[1,1]), size(A1, 1))
+    A1 - UniformScalingMap(convert(promote_type(T, eltype(A2)), A2.λ), size(A1, 1))
 Base.:(-)(A1::UniformScaling, A2::LinearMap{T}) where {T} =
-    UniformScalingMap(convert(T, A1[1,1]), size(A2, 1)) - A2
+    UniformScalingMap(convert(promote_type(T, eltype(A2)), A1.λ), size(A2, 1)) - A2
