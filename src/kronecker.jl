e2ea192c23410776c937b27d622de9cdfc462610
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-08-28 17:04:40 +0200

    add comments, use mixed-product trick

--- src/kronecker.jl
+++ src/kronecker.jl
@@ -12,12 +12,17 @@ KroneckerMap{T}(maps::As) where {T, As<:Tuple{LinearMap,LinearMap}} = KroneckerM
 Base.kron(A::LinearMap{TA}, B::LinearMap{TB}) where {TA,TB} = KroneckerMap{promote_type(TA,TB)}((A, B))
 Base.kron(A::LinearMap, B::AbstractArray) = kron(A, LinearMap(B))
 Base.kron(A::AbstractArray, B::LinearMap) = kron(LinearMap(A), B)
+# promote AbstractMatrix arguments to LinearMaps, then take LinearMap-Kronecker product
 for k in 3:8 # is 8 sufficient?
     Is = ntuple(n->:($(Symbol(:A,n))::AbstractMatrix), Val(k-1))
+    # yields (:A1, :A2, :A3, ..., :A(k-1))
     L = :($(Symbol(:A,k))::LinearMap)
+    # yields :Ak
     mapargs = ntuple(n -> :(LinearMap($(Symbol(:A,n)))), Val(k-1))
+    # yields (:LinearMap(A1), :LinearMap(A2), ..., LinearMap(A(k-1)))
 
-    @eval Base.kron($(Is...), $L, As::Union{LinearMap,AbstractMatrix}...) = kron($(mapargs...), $(Symbol(:A,k)), As...)
+    @eval Base.kron($(Is...), $L, As::Union{LinearMap,AbstractMatrix}...) =
+        kron($(mapargs...), $(Symbol(:A,k)), As...)
 end
 
 Base.size(A::KroneckerMap) = size(A.maps[1]) .* size(A.maps[2])
@@ -28,6 +33,14 @@ LinearAlgebra.ishermitian(A::KroneckerMap) = all(ishermitian, A.maps)
 
 LinearAlgebra.adjoint(A::KroneckerMap{T}) where {T} = KroneckerMap{T}(map(adjoint, A.maps))
 LinearAlgebra.transpose(A::KroneckerMap{T}) where {T} = KroneckerMap{T}(map(transpose, A.maps))
+
+function Base.:(*)(A::KroneckerMap, B::KroneckerMap)
+    if (size(A.maps[1], 2) == size(B.maps[1], 1) && size(A.maps[2], 2) == size(B.maps[2], 1))
+        return kron(A.maps[1]*B.maps[1], A.maps[2]*B.maps[2])
+    else
+        return CompositeMap{T}(tuple(B, A))
+    end
+end
 
 Base.:(==)(A::KroneckerMap, B::KroneckerMap) = (eltype(A) == eltype(B) && A.maps == B.maps)
 
