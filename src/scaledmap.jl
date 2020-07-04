50682a39a87132dbf11e4b2f5653a55fab5d5a50
Jeff Fessler <fessler@umich.edu>
2020-07-04 12:18:32 -0400

    Comment "todos"

--- src/scaledmap.jl
+++ src/scaledmap.jl
@@ -8,8 +8,12 @@ but for simplicity this code focuses "solely" on Real or Complex LinearMaps.
 using LinearAlgebra: UniformScaling
 import LinearAlgebra: issymmetric, ishermitian, isposdef
 
-const RealOrComplex = Union{Real,Complex} # todo: redundant with composite.jl
-const LinearMapRC = LinearMap{<:RealOrComplex}
+#=
+todo: i would prefer to let "scale" be a different type than the map.
+e.g., if A is ComplexF32, allow scale to be Real so that multiplying
+by scale would require only a real multiply.  but i could not figure
+out how to get @inferred to pass when i had scale::S
+=#
 
 """
     ScaledMap{T, A<:LinearMapRC} <: LinearMap{T}
@@ -38,6 +42,8 @@ Base.size(A::ScaledMap) = size(A.map)
 Base.isreal(A::ScaledMap) = isreal(A.scale) && isreal(A.map)
 LinearAlgebra.issymmetric(A::ScaledMap) = isreal(A.scale) && issymmetric(A.map)
 LinearAlgebra.ishermitian(A::ScaledMap) = ishermitian(A.map)
+
+# subtle issue here that if map is Complex, then so is scale
 LinearAlgebra.isposdef(A::ScaledMap) =
     isposdef(A.map) && isreal(A.scale) && real(A.scale) > 0
 
@@ -48,34 +54,48 @@ Base.adjoint(A::ScaledMap) = ScaledMap(conj(A.scale), adjoint(A.map))
 Base.:(==)(A::ScaledMap, B::ScaledMap) =
     (eltype(A) == eltype(B) && A.map == B.map) && A.scale == B.scale
 
+# approximate comparison (because == for real scalars is dubious)
+Base.:(≈)(A::ScaledMap, B::ScaledMap) =
+    (eltype(A) == eltype(B) && A.map == B.map) && A.scale ≈ B.scale
+
+# x * conj(x) is real in math, but not perfectly so in computation,
+# but we want it to be real here (when possible) for isposdef()
+# and so that α*conj(α)*A is has a real scale when A is Real
+@inline _scalar_product(a, b) = isreal(a*b) ? real(a*b) : a*b
+
 # scalar multiplication and division
-Base.:(*)(α::RealOrComplex, A::ScaledMap) = ScaledMap(α * A.scale, A.map)
+Base.:(*)(α::RealOrComplex, A::ScaledMap) =
+    ScaledMap(_scalar_product(α, A.scale), A.map)
 Base.:(*)(α::RealOrComplex, A::LinearMapRC) = ScaledMap(α, A)
 Base.:(*)(A::LinearMapRC, α::RealOrComplex) = α * A
 Base.:(*)(A::ScaledMap, α::RealOrComplex) = α * A
+
+Base.:(*)(α::RealOrComplex, A::CompositeMapRC) = ScaledMap(α, A)
+Base.:(*)(A::CompositeMapRC, α::RealOrComplex) = ScaledMap(α, A)
 
 Base.:(\)(α::RealOrComplex, A::LinearMapRC) = inv(α) * A
 Base.:(/)(A::LinearMapRC, α::RealOrComplex) = inv(α) * A
 Base.:(-)(A::LinearMapRC) = -1 * A # possibly redundant with compositemap.jl
 
 # I
-# these two are superceded by the more general case below
-#Base.:(*)(A::ScaledMap, B::UniformScaling{<:RealOrComplex}) = B.λ * A
-#Base.:(*)(A::UniformScaling{<:RealOrComplex}, B::ScaledMap) = A.λ * B
 Base.:(*)(A::LinearMapRC, B::UniformScaling{<:RealOrComplex}) = B.λ * A
 Base.:(*)(A::UniformScaling{<:RealOrComplex}, B::LinearMapRC) = A.λ * B
 
 
 # composition (not essential, but might save multiple scaling operations)
 Base.:(*)(A::ScaledMap, B::ScaledMap) =
-     ScaledMap(A.scale * B.scale, A.map * B.map)
+     ScaledMap(_scalar_product(A.scale, B.scale), A.map * B.map)
 
+# composition: keep "scale" as the outer-most type
+# this helps with cases like "transpose(F) * 3 * F"
+Base.:(*)(A::ScaledMap, B::LinearMapRC) = ScaledMap(A.scale, A.map * B)
+Base.:(*)(A::LinearMapRC, B::ScaledMap) = ScaledMap(B.scale, A * B.map)
 
 # multiplication with vectors
 function A_mul_B!(y::AbstractVector, A::ScaledMap, x::AbstractVector)
     # no size checking, will be done by map
     A_mul_B!(y, A.map, x)
-    y .*= A.scale
+    y .*= A.scale # todo: any more efficient way using 4-arg mul! ?
 end
 
 # todo: 5-arg mul! ?
