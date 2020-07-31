4c770c458d22db97433274f3fb5b1f2241d0562f
Jeff Fessler <fessler@umich.edu>
2020-07-30 22:48:42 -0400

    Try 1.0 again

--- test/left.jl
+++ test/left.jl
@@ -3,16 +3,16 @@ using LinearAlgebra: mul!
 
 # y'*L is an exception to the left multiplication rule that makes a WrappedMap
 
-@static if VERSION ≥ v"1.4.0"
+@static if true # VERSION ≥ v"1.4.0"
 
 function left_tester(L::LinearMap{T}) where {T}
-	M,N = size(L)
+    M,N = size(L)
     A = Matrix(L)
 
     x = rand(T, N)
     y = rand(T, M)
 
-	# *
+    # *
     @test y' * A ≈ y' * L
     @test (y' * A) * x ≈ y' * (L * x) # associative
     @test transpose(y) * A ≈ transpose(y) * L
@@ -32,24 +32,24 @@ function left_tester(L::LinearMap{T}) where {T}
     mul!(b2, transpose(y), L, true, false) # 5-arg
     @test b1 ≈ b2
 
-	true
+    true
 end
 
 
 @testset "left mul vec" begin
     T = ComplexF32
-	M,N = 5,5
-	L = LinearMap{T}(cumsum, reverse ∘ cumsum ∘ reverse, N)
+    M,N = 5,5
+    L = LinearMap{T}(cumsum, reverse ∘ cumsum ∘ reverse, N)
 
-	@test left_tester(L) # FunctionMap
-	@test left_tester(L'*L) # CompositeMap
-	@test left_tester(2L) # ScaledMap
-	@test left_tester(kron(L,L')) # KroneckerMap
-	@test left_tester(2L+3L') # LinearCombination
-	@test left_tester([L L]) # BlockMap
+    @test left_tester(L) # FunctionMap
+    @test left_tester(L'*L) # CompositeMap
+    @test left_tester(2L) # ScaledMap
+    @test left_tester(kron(L,L')) # KroneckerMap
+    @test left_tester(2L+3L') # LinearCombination
+    @test left_tester([L L]) # BlockMap
 
-	W = LinearMap(randn(T,5,4))
-	@test left_tester(W) # WrappedMap
+    W = LinearMap(randn(T,5,4))
+    @test left_tester(W) # WrappedMap
 end
 
 end # version
