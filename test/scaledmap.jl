2b71eed7e54ed576c332bc54de39dc5bf9b9d761
Jeff Fessler <JeffFessler@users.noreply.github.com>
2021-06-13 09:13:36 -0400

    Update test/scaledmap.jl
    Co-authored-by: Daniel Karrasch <daniel.karrasch@posteo.de>

--- test/scaledmap.jl
+++ test/scaledmap.jl
@@ -81,7 +81,6 @@ using Test, LinearMaps, LinearAlgebra, BenchmarkTools
     B = @inferred β * A
     C = @inferred B' * B
     @test !isposdef(C)
-    
     N = 2^8
     A0 = LinearMap{T}(cumsum, reverse ∘ cumsum ∘ reverse, N) # out-of-place
     forw! = cumsum!
