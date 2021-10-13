a311ec605d7d1b6770da4fc9f51e3a590b1bb1ed
Kristof Cools <krcools@gmail.com>
2021-10-13 16:26:44 +0200

    nontradaxes test do not depend on LiftedMaps

--- test/nontradaxes.jl
+++ test/nontradaxes.jl
@@ -1,4 +1,4 @@
-using Test, LinearMaps, LinearAlgebra, LiftedMaps, BlockArrays
+using Test, LinearMaps, LinearAlgebra, BlockArrays
 
 @testset "Non-traditional axes" begin
 
@@ -6,16 +6,14 @@ using Test, LinearMaps, LinearAlgebra, LiftedMaps, BlockArrays
     ax2 = blockedrange([3,4])
 
     A = rand(ComplexF64,2,4)
-    L = LiftedMap(A,Block(1), Block(2), ax1, ax2)
     B = similar(PseudoBlockMatrix{ComplexF64}, ax1, ax2)
     fill!(B,0)
     B[Block(1),Block(2)] .= A
 
-    M = @inferred LinearMap(L)
     N = @inferred LinearMap(B)
-    @test axes(M) == axes(N)
+    @test axes(N) == (ax1,ax2)
 
-    @test eltype(M) == eltype(L) == eltype(A)
+    @test eltype(N) == eltype(B)
 
     u = similar(Array{ComplexF64}, ax2)
     v = similar(Array{ComplexF64}, blockedrange([3,5]))
@@ -25,25 +23,20 @@ using Test, LinearMaps, LinearAlgebra, LiftedMaps, BlockArrays
     for i in eachindex(v) v[i] = rand(ComplexF64) end
     for i in eachindex(w) w[i] = rand(ComplexF64) end
 
-    @test L*u == N*u
-    @test_throws DimensionMismatch L*v
+    @test B*u == N*u
     @test_throws DimensionMismatch N*v
 
-    Lu = L*u
+    # Lu = L*u
     Nu = N*u
 
-    @test axes(Lu)[1] == axes(L)[1] == axes(M)[1]
     @test axes(Nu)[1] == axes(N)[1] == axes(B)[1]
-
-    @test blocksizes(axes(Lu)[1]) == blocksizes(axes(L)[1]) == ([2,3],)
     @test blocksizes(axes(Nu)[1]) == blocksizes(axes(N)[1]) == blocksizes(axes(B)[1]) == ([2,3],)
 
-    C = L + 2N
-    @test axes(C) === axes(L) === axes(N)
-    @test C*u ≈ Lu + 2*Nu
+    C = B + 2N
+    @test axes(C) === axes(B) === axes(N)
+    @test C*u ≈ 3*Nu
 
     Cu = C*u
     @test axes(C)[1] == ax1
     @test blocksizes(axes(C)[1]) == blocksizes(ax1)
-
 end
