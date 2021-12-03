d56060f5ec67f9d3714d4d97040d45725e012338
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-12-03 23:06:29 +0100

    simplify compiler macros

--- src/getindex.jl
+++ src/getindex.jl
@@ -4,31 +4,28 @@
 #     ScaledMap, UniformScalingMap, WrappedMap
 
 # required in Base.to_indices for [:]-indexing
-Base.eachindex(::IndexLinear, A::LinearMap) = (Base.@_inline_meta; Base.OneTo(length(A)))
-Base.lastindex(A::LinearMap) = (Base.@_inline_meta; last(eachindex(IndexLinear(), A)))
-Base.firstindex(A::LinearMap) = (Base.@_inline_meta; first(eachindex(IndexLinear(), A)))
+Base.eachindex(::IndexLinear, A::LinearMap) = Base.OneTo(length(A))
+Base.lastindex(A::LinearMap) = last(eachindex(IndexLinear(), A))
+Base.firstindex(A::LinearMap) = first(eachindex(IndexLinear(), A))
 
 function Base.checkbounds(A::LinearMap, i, j)
-    Base.@_inline_meta
     Base.checkbounds_indices(Bool, axes(A), (i, j)) || throw(BoundsError(A, (i, j)))
     nothing
 end
 # Linear indexing is explicitly allowed when there is only one (non-cartesian) index
 function Base.checkbounds(A::LinearMap, i)
-    Base.@_inline_meta
     Base.checkindex(Bool, Base.OneTo(length(A)), i) || throw(BoundsError(A, i))
     nothing
 end
 
 # main entry point
-Base.@propagate_inbounds function Base.getindex(A::LinearMap, I...)
+function Base.getindex(A::LinearMap, I...)
     # TODO: introduce some sort of switch?
-    Base.@_inline_meta
     @boundscheck checkbounds(A, I...)
-    @inbounds _getindex(A, Base.to_indices(A, I)...)
+    _getindex(A, Base.to_indices(A, I)...)
 end
 # quick pass forward
-Base.@propagate_inbounds Base.getindex(A::ScaledMap, I...) = A.λ .* getindex(A.lmap, I...)
+Base.@propagate_inbounds Base.getindex(A::ScaledMap, I...) = A.λ .* A.lmap[I...]
 Base.@propagate_inbounds Base.getindex(A::WrappedMap, I...) = A.lmap[I...]
 Base.@propagate_inbounds Base.getindex(A::WrappedMap, i::Integer) = A.lmap[i]
 Base.@propagate_inbounds Base.getindex(A::WrappedMap, i::Integer, j::Integer) = A.lmap[i,j]
@@ -36,28 +33,26 @@ Base.@propagate_inbounds Base.getindex(A::WrappedMap, i::Integer, j::Integer) = 
 ########################
 # linear indexing
 ########################
-Base.@propagate_inbounds function _getindex(A::LinearMap, i::Integer)
-    Base.@_inline_meta
+function _getindex(A::LinearMap, i::Integer)
     i1, i2 = Base._ind2sub(axes(A), i)
     return _getindex(A, i1, i2)
 end
-Base.@propagate_inbounds _getindex(A::LinearMap, I::AbstractVector{<:Integer}) =
-    [_getindex(A, i) for i in I]
+_getindex(A::LinearMap, I::AbstractVector{<:Integer}) = [_getindex(A, i) for i in I]
 _getindex(A::LinearMap, ::Base.Slice) = vec(Matrix(A))
 
 ########################
 # Cartesian indexing
 ########################
-Base.@propagate_inbounds _getindex(A::LinearMap, i::Integer, j::Integer) =
-    _getindex(A, Base.Slice(axes(A)[1]), j)[i]
-Base.@propagate_inbounds function _getindex(A::LinearMap, i::Integer, J::AbstractVector{<:Integer})
+_getindex(A::LinearMap, i::Integer, j::Integer) =
+    @inbounds _getindex(A, Base.Slice(axes(A)[1]), j)[i]
+function _getindex(A::LinearMap, i::Integer, J::AbstractVector{<:Integer})
     try
-        return (basevec(A, i)'A)[J]
+        return @inbounds (basevec(A, i)'A)[J]
     catch
         x = zeros(eltype(A), size(A, 2))
         y = similar(x, eltype(A), size(A, 1))
         r = similar(x, eltype(A), length(J))
-        for (ind, j) in enumerate(J)
+        @inbounds for (ind, j) in enumerate(J)
             x[j] = one(eltype(A))
             _unsafe_mul!(y, A, x)
             r[ind] = y[i]
@@ -73,10 +68,10 @@ function _getindex(A::LinearMap, i::Integer, J::Base.Slice)
         return vec(_getindex(A, i:i, J))
     end
 end
-Base.@propagate_inbounds _getindex(A::LinearMap, I::AbstractVector{<:Integer}, j::Integer) =
-    _getindex(A, Base.Slice(axes(A)[1]), j)[I] # = A[:,j][I] w/o bounds check
+_getindex(A::LinearMap, I::AbstractVector{<:Integer}, j::Integer) =
+    @inbounds _getindex(A, Base.Slice(axes(A)[1]), j)[I] # = A[:,j][I] w/o bounds check
 _getindex(A::LinearMap, ::Base.Slice, j::Integer) = A*basevec(A, j)
-Base.@propagate_inbounds function _getindex(A::LinearMap, Is::Vararg{AbstractVector{<:Integer},2})
+function _getindex(A::LinearMap, Is::Vararg{AbstractVector{<:Integer},2})
     shape = Base.index_shape(Is...)
     dest = zeros(eltype(A), shape)
     I, J = Is
@@ -86,11 +81,11 @@ Base.@propagate_inbounds function _getindex(A::LinearMap, Is::Vararg{AbstractVec
     end
     return dest
 end
-Base.@propagate_inbounds function _getindex(A::LinearMap, I::AbstractVector{<:Integer}, ::Base.Slice)
+function _getindex(A::LinearMap, I::AbstractVector{<:Integer}, ::Base.Slice)
     x = zeros(eltype(A), size(A, 2))
     y = similar(x, eltype(A), size(A, 1))
     r = similar(x, eltype(A), (length(I), size(A, 2)))
-    @views for j in axes(A)[2]
+    @inbounds @views for j in axes(A)[2]
         x[j] = one(eltype(A))
         _unsafe_mul!(y, A, x)
         r[:,j] .= y[I]
@@ -98,10 +93,10 @@ Base.@propagate_inbounds function _getindex(A::LinearMap, I::AbstractVector{<:In
     end
     return r
 end
-Base.@propagate_inbounds function _getindex(A::LinearMap, ::Base.Slice, J::AbstractVector{<:Integer})
+function _getindex(A::LinearMap, ::Base.Slice, J::AbstractVector{<:Integer})
     x = zeros(eltype(A), size(A, 2))
     y = similar(x, eltype(A), (size(A, 1), length(J)))
-    for (i, j) in enumerate(J)
+    @inbounds for (i, j) in enumerate(J)
         x[j] = one(eltype(A))
         _unsafe_mul!(selectdim(y, 2, i), A, x)
         x[j] = zero(eltype(A))
@@ -112,12 +107,10 @@ _getindex(A::LinearMap, ::Base.Slice, ::Base.Slice) = Matrix(A)
 
 # specialized methods
 _getindex(A::FillMap, ::Integer, ::Integer) = A.λ
-Base.@propagate_inbounds _getindex(A::LinearCombination, i::Integer, j::Integer) =
-    sum(a -> A.maps[a][i, j], eachindex(A.maps))
-Base.@propagate_inbounds _getindex(A::AdjointMap, i::Integer, j::Integer) =
-    adjoint(A.lmap[j, i])
-Base.@propagate_inbounds _getindex(A::TransposeMap, i::Integer, j::Integer) =
-    transpose(A.lmap[j, i])
+_getindex(A::LinearCombination, i::Integer, j::Integer) =
+    sum(a -> (@inbounds A.maps[a][i, j]), eachindex(A.maps))
+_getindex(A::AdjointMap, i::Integer, j::Integer) = @inbounds adjoint(A.lmap[j, i])
+_getindex(A::TransposeMap, i::Integer, j::Integer) = @inbounds transpose(A.lmap[j, i])
 _getindex(A::UniformScalingMap, i::Integer, j::Integer) = ifelse(i == j, A.λ, zero(eltype(A)))
 
 # helpers
