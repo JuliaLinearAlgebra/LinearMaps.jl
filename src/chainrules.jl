6a15a30dd073f07061d10c013e0fb5b2d4ad5acb
Daniel Karrasch <Daniel.Karrasch@posteo.de>
2022-09-06 17:52:39 +0200

    indentation

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,9 +1,9 @@
 function rrule(::typeof(*), A::LinearMap, x::AbstractVector)
     y = A*x
     function pullback(dy)
-      DY = unthunk(dy)
-      # Because A is an abstract map, the product is only differentiable w.r.t the input
-      return NoTangent(), NoTangent(), @thunk(A' * DY)
+        DY = unthunk(dy)
+        # Because A is an abstract map, the product is only differentiable w.r.t the input
+        return NoTangent(), NoTangent(), @thunk(A' * DY)
     end
     return y, pullback
 end
@@ -11,9 +11,9 @@ end
 function rrule(A::LinearMap, x::AbstractVector)
     y = A*x
     function pullback(dy)
-      DY = unthunk(dy)
-      # Because A is an abstract map, the product is only differentiable w.r.t the input
-      return NoTangent(), @thunk(A' * DY)
+        DY = unthunk(dy)
+        # Because A is an abstract map, the product is only differentiable w.r.t the input
+        return NoTangent(), @thunk(A' * DY)
     end
     return y, pullback
 end
