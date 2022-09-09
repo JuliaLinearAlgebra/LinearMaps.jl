9798e124279c9274e6d59467d3eb6f28fbd711a2
Daniel Karrasch <Daniel.Karrasch@posteo.de>
2022-09-09 13:55:37 +0200

    simpler test

--- test/kronecker.jl
+++ test/kronecker.jl
@@ -38,7 +38,7 @@ using Test, LinearMaps, LinearAlgebra, SparseArrays
             @test L * v ≈ M * v
         end
         L = ones(3) ⊗ (b = rand(ComplexF64, 4))
-        @test L * [2] ≈ kron(ones(3), b) * 2
+        @test Matrix(L) == kron(ones(3), b)
         L = ones(3) ⊗ ones(ComplexF64, 4)'
         v = rand(4)
         @test Matrix(L) == ones(3,4)
